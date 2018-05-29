f8a61a61e0ec9affae1f511a3097c6540cf90591
Gabriel Fedel <gabriel.fedel@lnls.br>
2018-05-29 16:51:22 -0300

    run-appliance-images: change to use only 1 appliance

--- docker-appliance-images-single/run-appliance-images.sh
+++ docker-appliance-images-single/run-appliance-images.sh
@@ -2,22 +2,18 @@
 
 . ./env-vars-single.sh
 
-for APPLIANCE in "engine" "retrieval" "etl" "mgmt" 
-do
 
-        CONTAINERS=$(docker ps -a | grep ${DOCKER_RUN_NAME}-${APPLIANCE})
+CONTAINERS=$(docker ps -a | grep ${DOCKER_RUN_NAME})
 
-        if [ ! -z "$CONTAINERS" ]; then
-            docker stop ${DOCKER_RUN_NAME}-${APPLIANCE}
-            docker rm ${DOCKER_RUN_NAME}-${APPLIANCE}
-        fi
+    if [ ! -z "$CONTAINERS" ]; then
+        docker stop ${DOCKER_RUN_NAME}
+        docker rm ${DOCKER_RUN_NAME}
+    fi
 
-        APPLIANCE_PORT=$(xmlstarlet sel -t -v "/appliances/appliance[identity='${ARCHAPPL_MYIDENTITY}']/${APPLIANCE}_url" ../lnls_appliances.xml | sed "s/.*://" | sed "s/\/.*//" )
-
-        echo ${APPLIANCE_PORT} - ${APPLIANCE}
-
-        docker run -d --name=${DOCKER_RUN_NAME}-${APPLIANCE} --dns=10.0.0.71 --dns=10.0.0.72 \
-            -p ${APPLIANCE_PORT}:${APPLIANCE_PORT} --network=${NETWORK_ID} \
-            --volumes-from=${SHORT_TERM_VOLUME_NAME} --volumes-from=${MEDIUM_TERM_VOLUME_NAME} --volumes-from=${LONG_TERM_VOLUME_NAME} \
-            ${DOCKER_MANTAINER_NAME}/${DOCKER_NAME}-${APPLIANCE}:${DOCKER_TAG}
-done
+    APPLIANCE_PORT_MGMT=11995
+    APPLIANCE_PORT_RET=11998
+    docker run -d --name=${DOCKER_RUN_NAME} --dns=10.0.0.71 --dns=10.0.0.72 \
+        -p ${APPLIANCE_PORT_MGMT}:${APPLIANCE_PORT_MGMT} -p ${APPLIANCE_PORT_RET}:${APPLIANCE_PORT_RET} --network=${NETWORK_ID} \
+        --volumes-from=${SHORT_TERM_VOLUME_NAME} --volumes-from=${MEDIUM_TERM_VOLUME_NAME} --volumes-from=${LONG_TERM_VOLUME_NAME} \
+        -v /opt/epics-archiver-appliances/configuration/:/opt/epics-archiver-appliances/configuration/ \
+    --env-file lnls-epics-archiver.env  ${DOCKER_MANTAINER_NAME}/${DOCKER_NAME}:${DOCKER_TAG}
