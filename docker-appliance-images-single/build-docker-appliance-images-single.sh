4bf572f5e4cb4411f1ef9c82726277aa0201ec48
Gabriel Fedel <gabriel.fedel@lnls.br>
2018-05-29 16:43:59 -0300

    build single: create volumes for storage

--- docker-appliance-images-single/build-docker-appliance-images-single.sh
+++ docker-appliance-images-single/build-docker-appliance-images-single.sh
@@ -12,6 +12,28 @@
 
 . ./env-vars-single.sh
 
+### Build STS, MTS and LTS volumes
+
+DOCKER_STS_CONTAINER=$(docker ps -a | grep ${SHORT_TERM_VOLUME_NAME})
+
+if [ -z ${DOCKER_STS_CONTAINER:+x} ]; then
+    echo "${SHORT_TERM_VOLUME_FOLDER} has not been created. Creating... "
+    docker create -v ${SHORT_TERM_VOLUME_FOLDER} --name ${SHORT_TERM_VOLUME_NAME} debian &> /dev/null
+fi
+
+DOCKER_MTS_CONTAINER=$(docker ps -a | grep ${MEDIUM_TERM_VOLUME_NAME})
+if [ -z ${DOCKER_MTS_CONTAINER:+x} ]; then   
+    echo "${MEDIUM_TERM_VOLUME_FOLDER} has not been created. Creating... "
+    docker create -v ${MEDIUM_TERM_VOLUME_FOLDER} --name ${MEDIUM_TERM_VOLUME_NAME} debian &> /dev/null
+fi
+
+DOCKER_LTS_CONTAINER=$(docker ps -a | grep ${LONG_TERM_VOLUME_NAME})
+if [ -z ${DOCKER_LTS_CONTAINER:+x} ]; then
+    echo "${LONG_TERM_VOLUME_FOLDER} has not been created. Creating... "
+    docker create -v ${LONG_TERM_VOLUME_FOLDER} --name ${LONG_TERM_VOLUME_NAME} debian &> /dev/null
+fi
+
+
 set -x
 docker build -t ${DOCKER_MANTAINER_NAME}/${DOCKER_NAME}:${DOCKER_TAG} .
 
