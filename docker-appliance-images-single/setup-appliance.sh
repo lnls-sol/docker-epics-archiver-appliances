4e788b0827beef7f167d2f29749d33d9e8ff7af0
Gabriel Fedel <gabriel.fedel@lnls.br>
2018-06-05 13:16:48 -0300

    setup-applicance: remove everything from authentication

--- docker-appliance-images-single/setup-appliance.sh
+++ docker-appliance-images-single/setup-appliance.sh
@@ -21,14 +21,6 @@ IP_ADDRESS=$(hostname)
 # For debugging
 # sed -i "s:INFO:ALL:g" ${GITHUB_REPOSITORY_FOLDER}/src/sitespecific/lnls-control-archiver/classpathfiles/log4j.properties
 
-if [ "${USE_AUTHENTICATION}" = true ]; then
-        GITHUB_APPLIANCES_BRANCH=${GITHUB_APPLIANCES_BRANCH:-ldap-login}
-else
-        GITHUB_APPLIANCES_BRANCH=${GITHUB_APPLIANCES_BRANCH:-master}
-fi
-
-(cd ${GITHUB_REPOSITORY_FOLDER}; git config user.email "controle@lnls.br"; git config user.name "Controls Group"; git fetch origin ${GITHUB_APPLIANCES_BRANCH}; git checkout ${GITHUB_APPLIANCES_BRANCH})
-
 for APPLIANCE_UNIT in "mgmt" "engine" "retrieval" "etl"
 do
 
@@ -64,79 +56,79 @@ do
             # Sets cluster inet port
             xmlstarlet ed -L -u "/appliances/appliance[identity='${ARCHAPPL_MYIDENTITY}']/cluster_inetport" -v "${IP_ADDRESS}:12000" ${ARCHAPPL_APPLIANCES}
 
-            if [ "${USE_AUTHENTICATION}" = true ]; then
+#            if [ "${USE_AUTHENTICATION}" = true ]; then
+#
+#                # Generates keystore
+#                keytool -genkey -alias tomcat -keyalg RSA -dname "CN=${IP_ADDRESS}, OU=Controls Group, O=LNLS, L=Campinas, ST=Sao Paulo, C=BR" -storepass ${CERTIFICATE_PASSWORD} -keypass ${CERTIFICATE_PASSWORD} -keystore ${APPLIANCE_FOLDER}/build/cert/appliance-mgmt.keystore -validity 730
+#                # Copies keystore to conf/
+#                cp ${APPLIANCE_FOLDER}/build/cert/appliance-mgmt.keystore ${CATALINA_HOME}/conf/
+#                # Generates certificate
+#                keytool -exportcert -keystore conf/appliance-mgmt.keystore -alias tomcat -storepass ${CERTIFICATE_PASSWORD} -file ${APPLIANCE_FOLDER}/build/cert/archiver-mgmt.crt
+#                # Imports certificate into trusted keystore
+#                keytool -import -alias tomcat -trustcacerts -storepass ${CERTIFICATE_PASSWORD} -noprompt -keystore $JAVA_HOME/lib/security/cacerts -file ${APPLIANCE_FOLDER}/build/cert/archiver-mgmt.crt
+#
+#                xmlstarlet ed -L -u "/appliances/appliance[identity='${ARCHAPPL_MYIDENTITY}']/${APPLIANCE_UNIT}_url" -v "https://${IP_ADDRESS}:${APPLIANCE_PORT}/${APPLIANCE_UNIT}/bpl" ${ARCHAPPL_APPLIANCES}
+#
+#                # Remove default connector port
+#                xmlstarlet ed -L -d "/Server/Service/Connector" ${CATALINA_HOME}/${APPLIANCE_UNIT}/conf/server.xml
+#
+#                # Appends new connector
+#                xmlstarlet ed -L -s "/Server/Service" -t elem -n "Connector" \
+#                                 -i "/Server/Service/Connector" -t attr -n "protocol" -v "org.apache.coyote.http11.Http11NioProtocol" \
+#                                 -i "/Server/Service/Connector" -t attr -n "port" -v "${APPLIANCE_PORT}" \
+#                                 -i "/Server/Service/Connector" -t attr -n "redirectPort" -v "8443" \
+#                                 -i "/Server/Service/Connector" -t attr -n "maxThreads" -v "150" \
+#                                 -i "/Server/Service/Connector" -t attr -n "SSLEnabled" -v "true" \
+#                                 -i "/Server/Service/Connector" -t attr -n "scheme" -v "https" \
+#                                 -i "/Server/Service/Connector" -t attr -n "secure" -v "true" \
+#                                 ${CATALINA_HOME}/${APPLIANCE_UNIT}/conf/server.xml 
+#
+#                 xmlstarlet ed -L -s '/Server/Service/Connector[@port='"${APPLIANCE_PORT}"']' -t elem -n "SSLHostConfig" ${CATALINA_HOME}/${APPLIANCE_UNIT}/conf/server.xml
+#
+#                 cp ${APPLIANCE_FOLDER}/build/cert/appliance-mgmt.keystore ${CATALINA_HOME}/${APPLIANCE_UNIT}/conf
+#
+#                 xmlstarlet ed -L -s '/Server/Service/Connector[@port='"${APPLIANCE_PORT}"']/SSLHostConfig' -t elem -n "Certificate" \
+#                                  -i '/Server/Service/Connector[@port='"${APPLIANCE_PORT}"']/SSLHostConfig/Certificate' -t attr -n "certificateKeystoreFile" -v "conf/appliance-mgmt.keystore" \
+#                                  -i '/Server/Service/Connector[@port='"${APPLIANCE_PORT}"']/SSLHostConfig/Certificate' -t attr -n "type" -v "RSA" \
+#                                  ${CATALINA_HOME}/${APPLIANCE_UNIT}/conf/server.xml
+#
+#                 # Appends new realm
+#                 xmlstarlet ed -L -s '/Server/Service/Engine/Host' -t elem -n "Realm" \
+#                                  -i '/Server/Service/Engine/Host/Realm' -t attr -n "connectionURL" -v "${CONNECTION_URL}" \
+#                                  -i '/Server/Service/Engine/Host/Realm' -t attr -n "userSearch" -v "${CONNECTION_USER_FILTER}" \
+#                                  -i '/Server/Service/Engine/Host/Realm' -t attr -n "userSubtree" -v "true" \
+#                                  -i '/Server/Service/Engine/Host/Realm' -t attr -n "userBase" -v "${CONNECTION_USER_BASE}" \
+#                                  -i '/Server/Service/Engine/Host/Realm' -t attr -n "className" -v "org.apache.catalina.realm.JNDIRealm" \
+#                                  ${CATALINA_HOME}/${APPLIANCE_UNIT}/conf/server.xml
 
-                # Generates keystore
-                keytool -genkey -alias tomcat -keyalg RSA -dname "CN=${IP_ADDRESS}, OU=Controls Group, O=LNLS, L=Campinas, ST=Sao Paulo, C=BR" -storepass ${CERTIFICATE_PASSWORD} -keypass ${CERTIFICATE_PASSWORD} -keystore ${APPLIANCE_FOLDER}/build/cert/appliance-mgmt.keystore -validity 730
-                # Copies keystore to conf/
-                cp ${APPLIANCE_FOLDER}/build/cert/appliance-mgmt.keystore ${CATALINA_HOME}/conf/
-                # Generates certificate
-                keytool -exportcert -keystore conf/appliance-mgmt.keystore -alias tomcat -storepass ${CERTIFICATE_PASSWORD} -file ${APPLIANCE_FOLDER}/build/cert/archiver-mgmt.crt
-                # Imports certificate into trusted keystore
-                keytool -import -alias tomcat -trustcacerts -storepass ${CERTIFICATE_PASSWORD} -noprompt -keystore $JAVA_HOME/lib/security/cacerts -file ${APPLIANCE_FOLDER}/build/cert/archiver-mgmt.crt
-
-                xmlstarlet ed -L -u "/appliances/appliance[identity='${ARCHAPPL_MYIDENTITY}']/${APPLIANCE_UNIT}_url" -v "https://${IP_ADDRESS}:${APPLIANCE_PORT}/${APPLIANCE_UNIT}/bpl" ${ARCHAPPL_APPLIANCES}
-
-                # Remove default connector port
-                xmlstarlet ed -L -d "/Server/Service/Connector" ${CATALINA_HOME}/${APPLIANCE_UNIT}/conf/server.xml
-
-                # Appends new connector
-                xmlstarlet ed -L -s "/Server/Service" -t elem -n "Connector" \
-                                 -i "/Server/Service/Connector" -t attr -n "protocol" -v "org.apache.coyote.http11.Http11NioProtocol" \
-                                 -i "/Server/Service/Connector" -t attr -n "port" -v "${APPLIANCE_PORT}" \
-                                 -i "/Server/Service/Connector" -t attr -n "redirectPort" -v "8443" \
-                                 -i "/Server/Service/Connector" -t attr -n "maxThreads" -v "150" \
-                                 -i "/Server/Service/Connector" -t attr -n "SSLEnabled" -v "true" \
-                                 -i "/Server/Service/Connector" -t attr -n "scheme" -v "https" \
-                                 -i "/Server/Service/Connector" -t attr -n "secure" -v "true" \
-                                 ${CATALINA_HOME}/${APPLIANCE_UNIT}/conf/server.xml 
-
-                 xmlstarlet ed -L -s '/Server/Service/Connector[@port='"${APPLIANCE_PORT}"']' -t elem -n "SSLHostConfig" ${CATALINA_HOME}/${APPLIANCE_UNIT}/conf/server.xml
-
-                 cp ${APPLIANCE_FOLDER}/build/cert/appliance-mgmt.keystore ${CATALINA_HOME}/${APPLIANCE_UNIT}/conf
-
-                 xmlstarlet ed -L -s '/Server/Service/Connector[@port='"${APPLIANCE_PORT}"']/SSLHostConfig' -t elem -n "Certificate" \
-                                  -i '/Server/Service/Connector[@port='"${APPLIANCE_PORT}"']/SSLHostConfig/Certificate' -t attr -n "certificateKeystoreFile" -v "conf/appliance-mgmt.keystore" \
-                                  -i '/Server/Service/Connector[@port='"${APPLIANCE_PORT}"']/SSLHostConfig/Certificate' -t attr -n "type" -v "RSA" \
-                                  ${CATALINA_HOME}/${APPLIANCE_UNIT}/conf/server.xml
-
-                 # Appends new realm
-                 xmlstarlet ed -L -s '/Server/Service/Engine/Host' -t elem -n "Realm" \
-                                  -i '/Server/Service/Engine/Host/Realm' -t attr -n "connectionURL" -v "${CONNECTION_URL}" \
-                                  -i '/Server/Service/Engine/Host/Realm' -t attr -n "userSearch" -v "${CONNECTION_USER_FILTER}" \
-                                  -i '/Server/Service/Engine/Host/Realm' -t attr -n "userSubtree" -v "true" \
-                                  -i '/Server/Service/Engine/Host/Realm' -t attr -n "userBase" -v "${CONNECTION_USER_BASE}" \
-                                  -i '/Server/Service/Engine/Host/Realm' -t attr -n "className" -v "org.apache.catalina.realm.JNDIRealm" \
-                                  ${CATALINA_HOME}/${APPLIANCE_UNIT}/conf/server.xml
-
-                 if [ ! -z ${ALTERNATIVE_URL+x} ]; then
-                     xmlstarlet ed -L -i '/Server/Service/Engine/Host/Realm' -t attr -n "alternativeURL" -v "${ALTERNATIVE_URL}" \
-                                      ${CATALINA_HOME}/${APPLIANCE_UNIT}/conf/server.xml
-                 fi
-
-                 if [ ! -z ${CONNECTION_ROLE_BASE+x} ]; then
-                     xmlstarlet ed -L -i '/Server/Service/Engine/Host/Realm' -t attr -n "roleBase" -v "${CONNECTION_ROLE_BASE}" \
-                                      -i '/Server/Service/Engine/Host/Realm' -t attr -n "roleSubtree" -v "true" \
-                                      ${CATALINA_HOME}/${APPLIANCE_UNIT}/conf/server.xml
-                 fi
-
-                 if [ ! -z ${CONNECTION_ROLE_NAME+x} ]; then
-                     xmlstarlet ed -L -i '/Server/Service/Engine/Host/Realm' -t attr -n "roleName" -v "${CONNECTION_ROLE_NAME}" ${CATALINA_HOME}/${APPLIANCE_UNIT}/conf/server.xml
-                 fi
-
-                 if [ ! -z ${CONNECTION_ROLE_SEARCH+x} ]; then
-                     xmlstarlet ed -L -i '/Server/Service/Engine/Host/Realm' -t attr -n "roleSearch" -v "${CONNECTION_ROLE_SEARCH}" ${CATALINA_HOME}/${APPLIANCE_UNIT}/conf/server.xml
-                 fi
-
-                 if [ ! -z ${CONNECTION_NAME+x} ]; then
-                     xmlstarlet ed -L -i '/Server/Service/Engine/Host/Realm' -t attr -n "connectionName" -v "${CONNECTION_NAME}" ${CATALINA_HOME}/${APPLIANCE_UNIT}/conf/server.xml
-                 fi
-
-                 if [ !	-z ${CONNECTION_PASSWORD+x} ]; then
-                     xmlstarlet ed -L -i '/Server/Service/Engine/Host/Realm' -t attr -n "connectionPassword" -v "${CONNECTION_PASSWORD}" ${CATALINA_HOME}/${APPLIANCE_UNIT}/conf/server.xml
-                 fi 
-
-            else
+#                 if [ ! -z ${ALTERNATIVE_URL+x} ]; then
+#                     xmlstarlet ed -L -i '/Server/Service/Engine/Host/Realm' -t attr -n "alternativeURL" -v "${ALTERNATIVE_URL}" \
+#                                      ${CATALINA_HOME}/${APPLIANCE_UNIT}/conf/server.xml
+#                 fi
+#
+#                 if [ ! -z ${CONNECTION_ROLE_BASE+x} ]; then
+#                     xmlstarlet ed -L -i '/Server/Service/Engine/Host/Realm' -t attr -n "roleBase" -v "${CONNECTION_ROLE_BASE}" \
+#                                      -i '/Server/Service/Engine/Host/Realm' -t attr -n "roleSubtree" -v "true" \
+#                                      ${CATALINA_HOME}/${APPLIANCE_UNIT}/conf/server.xml
+#                 fi
+#
+#                 if [ ! -z ${CONNECTION_ROLE_NAME+x} ]; then
+#                     xmlstarlet ed -L -i '/Server/Service/Engine/Host/Realm' -t attr -n "roleName" -v "${CONNECTION_ROLE_NAME}" ${CATALINA_HOME}/${APPLIANCE_UNIT}/conf/server.xml
+#                 fi
+#
+#                 if [ ! -z ${CONNECTION_ROLE_SEARCH+x} ]; then
+#                     xmlstarlet ed -L -i '/Server/Service/Engine/Host/Realm' -t attr -n "roleSearch" -v "${CONNECTION_ROLE_SEARCH}" ${CATALINA_HOME}/${APPLIANCE_UNIT}/conf/server.xml
+#                 fi
+#
+#                 if [ ! -z ${CONNECTION_NAME+x} ]; then
+#                     xmlstarlet ed -L -i '/Server/Service/Engine/Host/Realm' -t attr -n "connectionName" -v "${CONNECTION_NAME}" ${CATALINA_HOME}/${APPLIANCE_UNIT}/conf/server.xml
+#                 fi
+#
+#                 if [ !	-z ${CONNECTION_PASSWORD+x} ]; then
+#                     xmlstarlet ed -L -i '/Server/Service/Engine/Host/Realm' -t attr -n "connectionPassword" -v "${CONNECTION_PASSWORD}" ${CATALINA_HOME}/${APPLIANCE_UNIT}/conf/server.xml
+#                 fi 
+#
+#            else
 
                 xmlstarlet ed -L -u "/appliances/appliance[identity='${ARCHAPPL_MYIDENTITY}']/${APPLIANCE_UNIT}_url" -v "http://${IP_ADDRESS}:${APPLIANCE_PORT}/${APPLIANCE_UNIT}/bpl" ${ARCHAPPL_APPLIANCES}
 
@@ -150,7 +142,7 @@ do
 	    # Changes viewer's url port
 	    RETRIEVAL_PORT=$(xmlstarlet sel -t -v "/appliances/appliance[identity='${ARCHAPPL_MYIDENTITY}']/retrieval_url" ${ARCHAPPL_APPLIANCES} | sed "s/.*://" | sed "s/\/.*//" ) 
 	    sed -i 's#var dataRetrievalURL = .*$#var dataRetrievalURL = window.location.port != "" \&\& window.location.port > 0 ? "http:" + window.location.href.split(":")[1] + ":'"${RETRIEVAL_PORT}"'/retrieval" :  "http://" + window.location.hostname + "/retrieval";#g' ${GITHUB_REPOSITORY_FOLDER}/src/main/org/epics/archiverappliance/mgmt/staticcontent/js/mgmt.js
-    fi
+#    fi
 
     # Change wardest and dist properties in build.xml to ./
     xmlstarlet ed -L -u "/project/property[@name='wardest']/@location" -v "./" ${GITHUB_REPOSITORY_FOLDER}/build.xml
