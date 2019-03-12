611285f2d8eff9780408fdbcba1d9dbac9b7077d
Gabriel Fedel <gabriel.fedel@lnls.br>
2019-03-12 11:04:46 -0300

    Create folders and copy configuration files
    Create necessary folders at /opt/epics-archiver-appliances and
    copy configuration files (lnls*) to there.

--- build-docker-generic-appliance.sh
+++ build-docker-generic-appliance.sh
@@ -11,4 +11,12 @@
 
 . ./env-vars.sh
 
+if [ ! -d "/opt/epics-archiver-appliances" ]; then
+    mkdir -p /opt/epics-archiver-appliances/configuration
+    mkdir -p /opt/epics-archiver-appliances/storage/sts
+    mkdir /opt/epics-archiver-appliances/storage/mts
+    mkdir /opt/epics-archiver-appliances/storage/lts
+    cp lnls* /opt/epics-archiver-appliances/configuration
+fi
+
 docker build -t ${DOCKER_MANTAINER_NAME}/${DOCKER_NAME}:${DOCKER_TAG} .
